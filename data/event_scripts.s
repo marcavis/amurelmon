68ac6908fad99f877e9c70dd9120a11b6fbfc34a
marcavis <marcavis@yahoo.com.br>
2026-05-20 23:02:46 -0300

    adicionando rotas 201 e 202, e a cidade de Santa Rosa de Lima

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1737,3 +1737,9 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/scripts/dexnav.inc"
 	.include "data/scripts/battle_frontier.inc"
 	.include "data/scripts/apricorn_tree.inc"
+
+	.include "data/maps/Route201/scripts.inc"
+
+	.include "data/maps/Route202/scripts.inc"
+
+	.include "data/maps/SantaRosaDeLima/scripts.inc"
